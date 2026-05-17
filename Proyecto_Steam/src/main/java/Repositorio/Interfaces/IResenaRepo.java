5bc7d6f9b63a02f6cb39b95db3c32d30df53b004
Derek <derek41198@gmail.com>
2026-05-17 22:53:04 +0200

    Delete Proyecto_Steam/src/main/java/Repositorio directory

--- Proyecto_Steam/src/main/java/Repositorio/Interfaces/IResenaRepo.java
+++
@@ -1,7 +0,0 @@
-package Repositorio.Interfaces;
-
-import Modelo.Entidad.EntidadResena;
-import Modelo.Form.FormResena;
-
-public interface IResenaRepo extends ICrud<EntidadResena, FormResena, Long> {
-}
